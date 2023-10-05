96d4e95390714edf5e04a5f9b9f7045f0d63c500
cesnew <cesnew11@gmail.com>
2023-10-05 13:56:58 -0600

    creacion de vista de login registro logout y recomendaciones

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from frontend import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup/',     views.signup,           name='signup'),
+    path('login/',      views.startsession,     name='login'),
+    path('logout/',     views.endsession,       name="logout"),
+    path('bandeja/',    views.bandeja,          name='bandeja'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm 
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form': UserCreationForm 
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                # usuario
+                login(request, user)
+                return redirect('login')
+            except:
+                # return HttpResponse('el usuario ya existe')
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error' : 'usario ya existe' 
+                })
+        # return HttpResponse('passwords no coinciden')
+        return render(request, 'signup.html', {
+            'form': UserCreationForm, 
+            'error': 'passwords no coinciden'
+    })
+
+
+def startsession(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'],
+        password=request.POST['password'])
+
+        if user is None:
+                return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'username or password incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('bandeja')
+        
+
+@login_required
+def endsession(request):
+    logout(request)
+    return redirect('index')
+
+
+def bandeja(request):
+    return render(request, 'recomendaciones.html')
